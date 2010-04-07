2a183f62ae66b0fd097dc74706cbec363bcfcf96
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2010-04-07 22:33:15 +0700

    added sound

--- KhangRobot/MyFirstRobot/src/KhangRobot/KhangRobot.java
+++ KhangRobot/MyFirstRobot/src/KhangRobot/KhangRobot.java
@@ -3,6 +3,9 @@ package KhangRobot;
 import KhangRobot.Strategy.RobotStrategy;
 import robocode.*;
 import java.awt.Color;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 /**
  * Rules set up:
@@ -14,11 +17,13 @@ import java.awt.Color;
  */
 public class KhangRobot extends AdvancedRobot {
     // the strategy - one strategy
+
     private RobotStrategy robotStrategy;
     private double opponentEnergy = 100;
     // We will try to change the strategy when it is long time and there is nothing happen
     public static final int NUMBER_OF_TICK = 700;
     private int tickNumber = NUMBER_OF_TICK;
+
     public RobotStrategy getRobotStrategy() {
         return robotStrategy;
     }
@@ -30,16 +35,19 @@ public class KhangRobot extends AdvancedRobot {
     public void setOpponentEnergy(double opponentEnergy) {
         this.opponentEnergy = opponentEnergy;
     }
+
     public void setAllAdjustTrue() {
         setAdjustGunForRobotTurn(true);
         setAdjustRadarForGunTurn(true);
         setAdjustRadarForRobotTurn(true);
     }
+
     public void setAllAdjustFalse() {
         setAdjustGunForRobotTurn(false);
         setAdjustRadarForGunTurn(false);
         setAdjustRadarForRobotTurn(false);
     }
+
     /**
      * Default behaviour
      */
@@ -51,22 +59,25 @@ public class KhangRobot extends AdvancedRobot {
         while (true) {
             robotStrategy.normalRun();
             setStrategy();
-            tickNumber --;
-            
+            tickNumber--;
+
             // make sure that if no event happens. We can stop the game
-            if(tickNumber == 0) {
+            if (tickNumber == 0) {
                 StrategyFactory.setIsTimedOut(true);
                 robotStrategy = StrategyFactory.generateNewStrategy(this);
                 resetTimeout();
             }
         }
     }
+
     public void resetTimeout() {
         tickNumber = NUMBER_OF_TICK;
     }
+
     private void setStrategy() {
         robotStrategy = StrategyFactory.generateNewStrategy(this);
     }
+
     /**
      * Found it, go ahead, ram him and rape him
      */
@@ -76,6 +87,7 @@ public class KhangRobot extends AdvancedRobot {
         robotStrategy.onScannedRobot(e);
         setStrategy();
     }
+
     @Override
     public void onBulletHit(BulletHitEvent event) {
         setOpponentEnergy(event.getEnergy());
@@ -87,6 +99,15 @@ public class KhangRobot extends AdvancedRobot {
      */
     @Override
     public void onHitByBullet(HitByBulletEvent e) {
+        try {
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(this.getClass().getResource("6.wav"));
+            Clip clip = AudioSystem.getClip();
+            clip.open(inputStream);
+            clip.loop(0);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         robotStrategy.onHitByBullet(e);
         setStrategy();
     }
